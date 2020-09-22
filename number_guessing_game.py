0a151bf4be75d3df01c6107339644cb8b792d7a3
Michael Dinsmore <52056199+michaeldinsmore92@users.noreply.github.com>
2020-09-21 18:50:28 -0700

    Update 9.21.20 @ 6:49p
    *Fixed error when entering a non-integer for the user name
    *Allowed for more space in between the code

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -9,17 +9,21 @@ import random
 import sys
 
 
+
 def start_game(highscore):
     # Store a random number as the answer/solution.
     choice = "y"
+    
     while choice == "y":
         ANSWER = random.randint(1,10)
         attempts = 1
+        
         if highscore == None:
             print("Currently, there is no high score! Set the stage!")
         else:    
             print("The current high score to beat is {} guesses. Good luck!".format(highscore))
         guess = input("I am thinking of a number between 1 and 10, including both. Take a guess! ")
+        
         # Continuously prompt the player for a guess.
         try:
             guess = int(guess)
@@ -28,7 +32,8 @@ def start_game(highscore):
             start_game(highscore)
         except TypeError as err:
             print("Uh oh! That's not a valid value. Please try again...".format(err))
-            start_game(highscore)  
+            start_game(highscore)
+              
         while guess != ANSWER:
             try:
                 if guess > 10 or guess < 1:
@@ -48,14 +53,15 @@ def start_game(highscore):
             except ValueError as err:
                 print("Uh oh! That's not a valid value. Please try again...".format(err))
             except TypeError as err:
-                print("Uh oh! That's not a valid value. Please try again...".format(err))            
+                print("Uh oh! That's not a valid value. Please try again...".format(err))
+                            
         #   Once the guess is correct, stop looping, inform the user they "Got it"
         if guess == ANSWER:
             print("You got it, {}!".format(name))
             print("It took you {} attempts.".format(attempts))
             if highscore == None or attempts < highscore:
                 highscore = attempts
-                print("Whoa! Congratulations! You set the high score! {} attempts".format(highscore))
+                print("Whoa! Congratulations! You set the high score! ({} attempts)".format(highscore))
                 choice = input("Would you like to play again? Y/N ")
                 if choice.lower() == "n":
                     print("Thank you for playing, {}. Goodbye!".format(name))
@@ -63,13 +69,15 @@ def start_game(highscore):
                 else:
                     start_game(highscore)
             else:
-                print("Try again to beat the high score! {}".format(highscore))
+                print("Try again to beat the high score! ({} attempts)".format(highscore))
                 choice = input("Would you like to play again? Y/N ")
                 if choice.lower() != "n":
                     start_game(highscore)
                 else:
                     print("Thank you for playing, {}. Goodbye!".format(name))
                     sys.exit()
+
+
 
 if __name__ == '__main__':
     # Display an intro/welcome message to the player.
